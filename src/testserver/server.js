a4dcef1800c17ae5f813a7c573fa8f78af4bf632
shanedanielwinand <shanewinand@gmail.com>
2022-01-06 22:28:21 -0800

    added Node.JS server code for backend

---
+++ src/testserver/server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var path = require('path')
+var fs = require('fs')
+var cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/', (req, res) => {
+	console.log(req.body)
+	res.sendStatus(200)
+	fs.writeFile(path.join(__dirname, 'test.txt'), (JSON.stringify(req.body) + '\n'), {flag: 'a'}, err => {})
+})
+
+app.get('/', (req, res) => {
+	res.sendStatus(200)
+	console.log("test")
+})
+
+console.log("Starting Server.")
+app.listen(80)
